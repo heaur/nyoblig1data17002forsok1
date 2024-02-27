4a92157bef6adeac5d47d5cd67297d3bb9d23655
heaur <143513292+heaur@users.noreply.github.com>
2024-02-27 12:56:44 +0100

    Initial commit

---
+++ src/main/java/com/example/nyoblig1data17002forsok/Nyoblig1data17002forsokApplication.java
@@ -0,0 +1,13 @@
+package com.example.nyoblig1data17002forsok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nyoblig1data17002forsokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nyoblig1data17002forsokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nyoblig1data17002forsok/Nyoblig1data17002forsokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nyoblig1data17002forsok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nyoblig1data17002forsokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
